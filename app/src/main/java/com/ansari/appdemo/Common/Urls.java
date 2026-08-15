2839a3c721528dcb5ae9a2d8b0eca80e262f8590
akshatabopche <akshatabopche@gmail.com>
2026-08-15 12:32:37 +0530

    Designed a login screen

---
+++ app/src/main/java/com/ansari/appdemo/Common/Urls.java
@@ -0,0 +1,16 @@
+package com.ansari.appdemo.Common;
+
+public class Urls {
+
+    public static final String BASE_URL =
+            "http://10.11.57.114/sanketsetuAPI/";
+
+    // Register User
+    public static final String REGISTER_URL =
+            BASE_URL + "register.php";
+
+    // Login User
+    public static final String LOGIN_URL =
+            BASE_URL + "login.php";
+
+}

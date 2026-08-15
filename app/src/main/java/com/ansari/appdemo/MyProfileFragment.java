5ef03986690100558c1d11f4442d7ba17659c807
himanichaudhari21 <chaudharihimani@36gmail.com>
2026-08-15 00:06:22 -0700

    create MyProfileFragment

--- app/src/main/java/com/ansari/appdemo/MyProfileFragment.java
+++ app/src/main/java/com/ansari/appdemo/MyProfileFragment.java
@@ -8,10 +8,8 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,8 +27,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-
-import java.io.IOException;
 
 
 public class MyProfileFragment extends Fragment {
